9aa0426fef0bd3ae3ae13ae477b379b2783608e8
Vikrant Kudesia <vvkudesia@gmail.com>
2016-03-03 01:30:01 -0500

    changed sprite sheet and updated styles sheet accordingly. added required attrib on select but kept validation styles off of it

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,30 +1,29 @@
     
-// when window or document ready
-// credit card validation information
+// credit card validation
 
 $('#credit-card-number__data').validateCreditCard(function(result){
-	if (result.card_type === null){ $('.credit-card__items input').prop("checked",false)
-}
-	else{ 
+	if (result.card_type === null) {
+		$('.credit-card__items input').prop("checked",false)
+	} else { 
 		$("#" + result.card_type.name).prop("checked", true);
 			showCSV();}
 });
 
 
 // [ GOTCHA ]
-// American Express cards have the csv in a different location
+// American Express cards have a 4-digit csv on the front of the card
 // So, if the American Express radio button is selected (card begins in 34 or 37)
 // we will have to switch out the csv-sprite__image, as well
-//
+// 
 
 function showCSV(){
 	var csv = $('.csv-sprite'); 			//get the csv-sprite class
-		if($('#amex').is(':checked')){	 	//but if the input is an amex
+		if ($('#amex').is(':checked')){	 	//but if the input is an amex
 			csv.addClass('amex');	 		//add the amex class to the csv-sprite class
-	} 
-		else{csv.removeClass('amex');		//otherwise, display just the csv-sprite class
+	} else { csv.removeClass('amex');		//remove amex class if it was previously selected
 }
 }
+
 
 //Modernizr script
 
